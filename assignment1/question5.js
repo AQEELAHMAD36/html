2b28846fc2a33cbcfb6204e9783b6f04e77877f2
AQEEL AHMAD <124144207+AQEELAHMAD36@users.noreply.github.com>
2023-02-02 17:08:32 +0500

    html file

--- assignment1/question5.js
+++ assignment1/question5.js
@@ -1,9 +1,8 @@
-function createBase(baseNumber) {
-    return function (numberToAdd) {
-        return baseNumber + numberToAdd;
-    }
-}
-var addSix = createBase(6);
-
-console.log(addSix(10)); // returns 16
-console.log(addSix(21));
+const getMean = (array) => {
+  let total = 0;
+  for (let item of array) {
+    total += item;
+  }
+  
+  return total / array.length;
+} 
