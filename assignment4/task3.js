3bc4317771a0974a82d4306242ebb27f11b2f8c5
AQEEL AHMAD <124144207+AQEELAHMAD36@users.noreply.github.com>
2023-02-07 17:18:53 +0500

    assignment4

---
+++ assignment4/task3.js
@@ -0,0 +1,19 @@
+var library = [
+    {
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true
+    },
+    {
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
+    readingStatus: true
+    },
+    {
+    author: 'Suzanne Collins',
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    readingStatus: false
+    }];
+    
+    library.forEach(book => console.log(`AUTHOR : ${book.author} NAME : ${book.title} Reading Status : ${book.readingStatus}`) ) ;
+    
